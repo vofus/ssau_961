4084496eedb543ec72437e7e49a5038e56bcc9f7
Vasin Andrey <vofus@yandex.ru>
2015-06-06 02:55:39 +0400

    Добавлен обработчик события при нажатии на ссылку 'Новости'

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-	$.getJSON('BD/line_news.json', function(data) {
+	$("#news").click(function() {
+		$.getJSON('BD/line_news.json', function(data) {
 		console.log(data);
 
 		var output = "<ul class='posts'>";
 		$.each(data, function(key, val) {
 			 output += "<li>";
 			 output += "<h2>" + val.title + "</h2>";
-			 output += "<img src='" + val.imageUrl + "' alt='" + val.title + "'>";
+			 output += "<img src='" + val.imageUrl + "' alt='" + val.title + "' width='600px'>";
 			 output += "<p>" + val.snippet + "</p>";
+			 output += "<hr>";
 			 output += "<a href='" + val.fail + "'>";
-			 output += "<i class='fa fa-file-archive-o'></i> Компьютерная графика";
+			 output += "<i class='fa fa-file-archive-o'></i>&nbsp;&nbsp;" + val.subject;
 			 output += "</a>";
 			 output += "</li>";
 		});
 		output += "</ul>";
 		$("#update").html(output);
+	});
 	});
 });
