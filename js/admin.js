d2968eb2d98fbc3c902938658f6c09159d19a7b5
Vasin Andrey <vofus@yandex.ru>
2015-06-06 17:01:28 +0400

    Установлен запрет на кэширование файла JSON для того, чтобы на клиенте он всегда был актуальным. Также создан шаблон админской формы(но там еще много чего предстоит сделать, пока все новости будут добавляться вручную через файл JSON)

---
+++ js/admin.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+// 	var items = $.getJSON('BD/line_news.json', function(data) {
+// 		console.log(data);
+// 	});
+	
+// 	$('#admin_form').submit(function(event) {
+// 		event.preventDefault();
+// 		var elem = {
+// 				title : $('#title').val(),
+// 				subject : $('#subject').val(),
+// 				imageUrl : $('#imgUrl').val(),
+// 				snippet : $('#text').val(),
+// 				fail : $('#file').val()
+// 			};
+// 		items[items.length] = elem;
+
+// 		console.log(items);
+// 	});
+// });
--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
+	// Запрет на кэширование JSON файла,
+	// чтобы клиент всегда получал актуальный файл
+	var params = {
+		cache: false
+	};
+	
 	$("#news").click(function() {
-		$.getJSON('BD/line_news.json', function(data) {
-		console.log(data);
+		$.getJSON('http://127.0.0.1:8080/ssau961.com/BD/line_news.json', params, function(data) {
+		//console.log(data);
 
 		var output = "<ul class='posts'>";
 		$.each(data, function(key, val) {
+			//console.log("Проход: " + (key+1));	
 			 output += "<li>";
 			 output += "<h2>" + val.title + "</h2>";
-			 output += "<img src='" + val.imageUrl + "' alt='" + val.title + "' width='600px'>";
+			 output += "<img src='" + val.imageUrl + "' alt='" + val.title + "' width='80%'>";
 			 output += "<p>" + val.snippet + "</p>";
 			 output += "<hr>";
-			 output += "<a href='" + val.fail + "'>";
-			 output += "<i class='fa fa-file-archive-o'></i>&nbsp;&nbsp;" + val.subject;
-			 output += "</a>";
+			 if (val.file !== "") {
+			 	output += "<a href='" + val.file + "'>";
+				 output += "<i class='fa fa-file-archive-o'></i>&nbsp;&nbsp;" + val.subject;
+				 output += "</a>";
+			 } else {
+			 	return false;
+			 }
 			 output += "</li>";
 		});
 		output += "</ul>";
